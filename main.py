f0479089ec9a170ecb5b7806cd757496685d03c6
Neeraj Prasad <nprasad@mit.edu>
2018-04-07 18:43:47 -0400

    modify fi"

--- main.py
+++ main.py
@@ -14,44 +14,44 @@ num_classes=2
 input_shape = (224,224,3)
 batch_size=60
 epochs=10
-dataset = 'melspectrograms'
+datasets_pre = ['melspectrograms', 'spectrograms', 'cwt_images']
 
 archs = networks.all_nets()
+for dataset in datasets_pre:
+	for m in archs:
 
-for m in archs:
-	print(m)
-	base_model = archs[m](input_shape)
-	top_model = networks.top_model(input_shape=base_model.output_shape[1:])
-	model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
-	model.compile(optimizer=optimizers.Adam(), 
-                      loss='binary_crossentropy', 
-                      metrics=['accuracy'])
-	print(os.getcwd())
-	training_generator, validation_generator = data_processing.get_gen(dataset)
+		base_model = archs[m](input_shape)
+		top_model = networks.top_model(input_shape=base_model.output_shape[1:])
+		model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
+		model.compile(optimizer=optimizers.SGD(lr=1e-4, momentum=0.9), 
+	                      loss='binary_crossentropy', 
+	                      metrics=['accuracy'])
+		
+		training_generator, validation_generator = data_processing.get_gen(dataset)
 
-	filepath = 'models/' + m + ".hdf5"
-	tensorboard = TensorBoard(log_dir="logs/" + m + '/')
+		filepath = 'models/' + m + ".hdf5"
+		tensorboard = TensorBoard(log_dir="logs/" + m + '/')
 
-	best_model_checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-	callbacks_list = [best_model_checkpoint]
+		best_model_checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+		callbacks_list = [best_model_checkpoint]
 
-	nb_training_samples = 0
-	nb_validation_samples = 0
+		nb_training_samples = 0
+		nb_validation_samples = 0
 
-	for ex in ['sick/', 'not_sick/']:
-		nb_training_samples += len([name for name in os.listdir('data/' + dataset + '/train/' + ex) if os.path.isfile('data/' + dataset + '/train/' + ex + name)])
-		nb_validation_samples += len([name for name in os.listdir('data/' + dataset + '/validation/' + ex) if os.path.isfile('data/' + dataset + '/validation/' + ex + name)])
+		for ex in ['sick/', 'not_sick/']:
+			nb_training_samples += len([name for name in os.listdir('data/' + dataset + '/train/' + ex) if os.path.isfile('data/' + dataset + '/train/' + ex + name)])
+			nb_validation_samples += len([name for name in os.listdir('data/' + dataset + '/validation/' + ex) if os.path.isfile('data/' + dataset + '/validation/' + ex + name)])
 
-	print(nb_training_samples, nb_validation_samples)
-
-	model.fit_generator(
-    	training_generator,
-    	steps_per_epoch=nb_training_samples/batch_size,
-    	epochs=epochs,
-    	validation_data=validation_generator,
-    	validation_steps=nb_validation_samples/batch_size,
-    	callbacks = callbacks_list,
-    	verbose=2)
+		print(nb_training_samples, nb_validation_samples)
+		print(m)
+		model.fit_generator(
+	    	training_generator,
+	    	steps_per_epoch=nb_training_samples/batch_size,
+	    	epochs=epochs,
+	    	validation_data=validation_generator,
+	    	validation_steps=nb_validation_samples/batch_size,
+	    	callbacks = callbacks_list,
+	    	verbose=2)
 
 
 
