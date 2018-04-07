615091977ba688f43a8190aceda95e9e059cdccb
Neeraj Prasad <nprasad@mit.edu>
2018-04-07 17:16:53 -0400

    chpath

--- launch.sh
+++ launch.sh
@@ -7,11 +7,7 @@
 #SBATCH -t 00:20:00
 #SBATCH --workdir=./subs/neeraj/
 
-module load openmind/singularity/2.4.5
-module add openmind/cuda/8.0
-module add openmind/cudnn/8.0-5.1
 
-PATH="/om/user/nprasad/angel-pfizer/"
 
 cd ..
 singularity exec -B /om:/om --nv /om/user/nprasad/singularity/belledon-tensorflow-keras-master-latest.simg \
