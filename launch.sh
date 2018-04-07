03be45e6ce1b48386cd25d6f1452a6a33ea263fd
Neeraj Prasad <nprasad@mit.edu>
2018-04-07 17:15:20 -0400

    new qu

--- launch.sh
+++ launch.sh
@@ -11,7 +11,7 @@ module load openmind/singularity/2.4.5
 module add openmind/cuda/8.0
 module add openmind/cudnn/8.0-5.1
 
-PATH=/om/user/nprasad/angel-pfizer/
+PATH="/om/user/nprasad/angel-pfizer/"
 
 cd ..
 singularity exec -B /om:/om --nv /om/user/nprasad/singularity/belledon-tensorflow-keras-master-latest.simg \
