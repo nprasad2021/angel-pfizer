70b2402d13daf341ef2ddfbb2ffc7cf3fc72899d
Neeraj Prasad <nprasad@mit.edu>
2018-04-07 14:26:25 -0400

    nets"

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from helper import data_processing, networks
+from keras.models import Model
 
 
 num_classes=2
 input_shape = (224,224,3)
 
-models = networks.all_nets()
+archs = networks.all_nets()
 
-for m in models:
+for m in archs:
+	print(m)
+	base_model = archs[m](num_classes, input_shape)
+	print(base_model.output_shape)
+	top_model = networks.top_model(input_shape=base_model.output_shape[1:])
+	model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
+	model.summary()
 
-	base_model = models[m](num_classes, input_shape)
-	base_model.summary()
+
+
+
+
+
+
+
+
+
