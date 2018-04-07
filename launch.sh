d6d0fe7bd6e3779c7b4b7e965c95404bb2488474
Neeraj Prasad <nprasad@mit.edu>
2018-04-07 16:36:18 -0400

    no qos jobs

--- launch.sh
+++ launch.sh
@@ -6,8 +6,6 @@
 #SBATCH --gres=gpu:titan-x:1
 #SBATCH -t 00:20:00
 #SBATCH --workdir=./subs/neeraj/
-#SBATCH --qos=cbmm
-
 
 cd ..
 singularity exec -B /om:/om --nv /om/user/nprasad/singularity/belledon-tensorflow-keras-master-latest.simg \
