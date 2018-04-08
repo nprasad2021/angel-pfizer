05ca62debbbdddf413962cda7d1b8c3a20b53991
Neeraj Prasad <nprasad@mit.edu>
2018-04-08 16:16:15 -0400

    update evaluation

---
+++ launch_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 4
+#SBATCH --array=0-2
+#SBATCH --job-name=new_exp
+#SBATCH --mem=10GB
+#SBATCH --gres=gpu:titan-x:1
+#SBATCH -t 03:00:00
+#SBATCH --workdir=./subs/ensemble/
+
+PATH_ANGEL="/om/user/nprasad/angel-pfizer"
+
+cd ..
+singularity exec -B /om:/om --nv /om/user/nprasad/singularity/belledon-tensorflow-keras-master-latest.simg \
+python /om/user/nprasad/angel-pfizer/main.py $PATH_ANGEL ${SLURM_ARRAY_TASK_ID}
